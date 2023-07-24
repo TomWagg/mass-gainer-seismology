1aa41c7e5b6e6f94afa9c79bb9a1eef5c28fa737
Tom Wagg <tomjwagg@gmail.com>
2023-07-24 09:54:22 +0200

    consistent colours and label stylings

--- helpers/mesaplot_tom.py
+++ helpers/mesaplot_tom.py
@@ -19,8 +19,11 @@ params = {'figure.figsize': (12, 8),
           'ytick.minor.size': 4}
 plt.rcParams.update(params)
 
+mass_gainer_col = plt.get_cmap("magma")(0.3)
+single_col = plt.get_cmap("magma")(0.8)
 
-def simple_hr(track=None, df=None, ylabel=r'Luminosity $\mathbf{\log_{10}(L/L_{\odot})}$',
+
+def simple_hr(track=None, df=None, ylabel=r'Luminosity $\log_{10}(\mathbf{L/L_{\odot}})$',
               cbar_var="center_he4", cbar_label=r"$X_{\rm He, center}$", trim_pre_ms=True,
               fig=None, ax=None, show=True, add_axes_info=False, plot_line=True, 
               cbar_loc=[0.38, 0.025, 0.6, 0.025], annotate_start=None, annotate_end=None,
@@ -54,7 +57,7 @@ def simple_hr(track=None, df=None, ylabel=r'Luminosity $\mathbf{\log_{10}(L/L_{\
     
     if new_fig or add_axes_info:
         ax.invert_xaxis()
-        ax.set_xlabel(r'Effective temperature $\mathbf{\log_{10}(T_{eff}/K)}$')
+        ax.set_xlabel(r'Effective temperature $\log_{10}(\mathbf{T_{eff}/K})$')
         ax.set_ylabel(ylabel)
 
         if cbar_var is not None:
@@ -67,13 +70,14 @@ def simple_hr(track=None, df=None, ylabel=r'Luminosity $\mathbf{\log_{10}(L/L_{\
     return fig, ax
 
 
-def plot_X_H_profile(age=None, X_c=None, tracks=None, mt_index=1, ref_index=2, fig=None, ax=None, show=True):
+def plot_X_H_profile(age=None, X_c=None, tracks=None, mt_index=1, ref_index=2, fig=None, ax=None, show=True,
+                     label_with="title"):
     if age is None and X_c is None:
         raise ValueError("At least one of `age` or `X_c` must not be None")
     if tracks is None:
         raise ValueError("`tracks` cannot be None")
     if fig is None or ax is None:
-        fig, ax = plt.subplots(figsize=(7, 4))
+        fig, ax = plt.subplots(figsize=(7, 3))
     plt.cla()
 
     if X_c is None:
@@ -83,17 +87,19 @@ def plot_X_H_profile(age=None, X_c=None, tracks=None, mt_index=1, ref_index=2, f
         acc_mod = np.abs(tracks[mt_index].history["center_h1"] - X_c).argmin()
         non_acc_mod = np.abs(tracks[ref_index].history["center_h1"] - X_c).argmin()
 
+    lw = 2
+
     ax.plot(tracks[mt_index].profiles[acc_mod]["mass"],
             tracks[mt_index].profiles[acc_mod]["x_mass_fraction_H"],
-             label="Had accretion", zorder=3)
+             label="Mass-gainer", zorder=3, color=mass_gainer_col, lw=lw)
     ax.plot(tracks[ref_index].profiles[non_acc_mod]["mass"],
             tracks[ref_index].profiles[non_acc_mod]["x_mass_fraction_H"],
-             label="No accretion")
+             label="Single", color=single_col, lw=lw)
 
-    ax.legend(loc="lower right")
+    ax.legend(loc="lower right", fontsize=0.4 * fs)
 
     ax.set_xlabel(r"Mass [$\rm M_{\odot}$]")
-    ax.set_ylabel("H mass fraction")
+    ax.set_ylabel("H mass fraction", fontsize=0.7*fs)
 
     m_fin = tracks[mt_index].history["star_mass"].iloc[-1]
     ax.annotate(f"Extends to {m_fin:1.1f} " + r"$\rm M_{\odot}$", xy=(1, 0.5), xytext=(0.95, 0.5), xycoords="axes fraction",
@@ -102,8 +108,12 @@ def plot_X_H_profile(age=None, X_c=None, tracks=None, mt_index=1, ref_index=2, f
     ax.set_ylim(0, 0.7)
     ax.set_xlim(0, 1.1)
     
-    ax.set_title(r"$X_H$ profile for a final mass " + f"~{m_fin:1.1f} " + r"$\rm M_{\odot}$ star"\
-                 + (f' at {age:1.1f} Myr' if X_c is None else r" with $X_c =$" + f' {X_c:1.2f}'))
+    if label_with == "title":
+        ax.set_title(r"$X_H$ profile for a final mass " + f"~{m_fin:1.1f} " + r"$\rm M_{\odot}$ star"\
+                    + (f' at {age:1.1f} Myr' if X_c is None else r" with $X_c =$" + f' {X_c:1.2f}'))
+    else:
+        ax.annotate(f'Age = {age:1.1f} Myr' if X_c is None else r"$X_c =$" + f' {X_c:1.2f}',
+                    xy=(0.02, 0.95), xycoords="axes fraction", va="top", fontsize=0.5*fs)
 
     plt.tight_layout()
 
@@ -115,7 +125,7 @@ def plot_X_H_profile(age=None, X_c=None, tracks=None, mt_index=1, ref_index=2, f
 
 def plot_BV_profile(age=None, X_c=None, tracks=None, mt_index=3, ref_index=2,
                     lw=1, x_scale="linear", fractional_mass=False, fill=True,
-                    fig=None, ax=None, show=True):
+                    fig=None, ax=None, show=True, label_with="title"):
     if age is None and X_c is None:
         raise ValueError("At least one of `age` or `X_c` must not be None")
     if tracks is None:
@@ -136,12 +146,12 @@ def plot_BV_profile(age=None, X_c=None, tracks=None, mt_index=3, ref_index=2,
 
     ax.set_yscale("log")
     ax.set_xscale(x_scale)
-    ax.set_ylim(1e0, 1e6)
+    ax.set_ylim(1e0, 1e4)
     
     for mod, track, tag, col in zip([acc_mod, non_acc_mod],
                                     [tracks[mt_index], tracks[ref_index]],
                                     ["Mass-gainer", "Single"],
-                                    [plt.get_cmap("magma")(0.3), plt.get_cmap("magma")(0.8)]):
+                                    [mass_gainer_col, single_col]):
         m = track.profiles[mod - 1]["mass"]
         if fractional_mass:
             m = m / m.max()
@@ -153,11 +163,15 @@ def plot_BV_profile(age=None, X_c=None, tracks=None, mt_index=3, ref_index=2,
 
     ax.set_ylabel("Brunt–Väisälä\n[Cycles per day]", fontsize=0.5 * fs)
     ax.set_xlabel(r"Mass $\mathbf{M/M_{\odot}}$")
-    ax.legend(loc="best", ncol=2, fontsize=0.4 * fs)
+    ax.legend(loc="upper right", ncol=2, fontsize=0.4 * fs)
     
     m_fin = tracks[mt_index].history["star_mass"].iloc[-1]
-    ax.set_title(r"Profile for a final mass " + f"~{m_fin:1.1f} " + r"$\rm M_{\odot}$ star"\
-                     + (f' at {age:1.1f} Myr' if X_c is None else r" with $X_c =$" + f' {X_c:1.2f}'))    
+    if label_with == "title":
+        ax.set_title(r"Profile for a final mass " + f"~{m_fin:1.1f} " + r"$\rm M_{\odot}$ star"\
+                        + (f' at {age:1.1f} Myr' if X_c is None else r" with $X_c =$" + f' {X_c:1.2f}'))
+    else:
+        ax.annotate(f'Age = {age:1.1f} Myr' if X_c is None else r"$X_c =$" + f' {X_c:1.2f}',
+                    xy=(0.02, 0.95), xycoords="axes fraction", va="top", fontsize=0.5*fs)
     
     plt.tight_layout()
     
